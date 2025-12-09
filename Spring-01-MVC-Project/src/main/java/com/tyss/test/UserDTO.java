80318d5dba0ea2b651db81686d64df0b8fed63cb
Kashi-S-A <kashappaappannavar@gmail.com>
2025-12-09 13:00:10 +0530

    done

---
+++ Spring-01-MVC-Project/src/main/java/com/tyss/test/UserDTO.java
@@ -0,0 +1,47 @@
+package com.tyss.test;
+
+public class UserDTO {
+
+	private String username;
+	private String useremail;
+	private long phone;
+	private String pwd;
+
+	@Override
+	public String toString() {
+		return "UserDTO [username=" + username + ", useremail=" + useremail + ", phone=" + phone + ", pwd=" + pwd + "]";
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUseremail() {
+		return useremail;
+	}
+
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
--- Spring-01-MVC-Project/src/main/java/com/tyss/test/controller/MsgController.java
+++ Spring-01-MVC-Project/src/main/java/com/tyss/test/controller/MsgController.java
@@ -1,12 +1,14 @@
 package com.tyss.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tyss.test.UserDTO;
 
 import jakarta.servlet.http.HttpServletRequest;
-
 
 @Controller
 public class MsgController {
@@ -17,32 +19,62 @@ public class MsgController {
 		System.out.println("Message API is triggered");
 		return "message.jsp";
 	}
-	
+
 	@RequestMapping("/greet")
 	public String greet() {
 		System.out.println("Hey , Good Afternoon");
 		return "greet.jsp";
 	}
-	
+
 	@RequestMapping("/reg")
-	public String getRegisterPage() {
-		System.out.println("returning register page");
+	public String getRegisterPage(Model model) {
+		model.addAttribute("dto", new UserDTO());
 		return "register.jsp";
 	}
-	
-	@RequestMapping(value = "/register",method = RequestMethod.POST)
-	public String registerUser(HttpServletRequest request) {
-		String un = request.getParameter("username");
-		String ue = request.getParameter("useremail");
-		String ph = request.getParameter("phone");
-		String pwd = request.getParameter("pwd");
-		
-		System.out.println(un);
-		System.out.println(ue);
-		System.out.println(ph);
-		System.out.println(pwd);
-		
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerUser(UserDTO dto) {
+
+		System.out.println(dto);
+
 		return "register.jsp";
 	}
-	
+
+	@RequestMapping("/data")
+	public String sendDataUsindModel(Model model) {
+
+		model.addAttribute("username", "Manga");
+		model.addAttribute("userage", 34);
+
+		return "display.jsp";
+	}
+
+	@RequestMapping("/send-data")
+	public ModelAndView sendDataUsingMAV() {
+
+		ModelAndView mv = new ModelAndView("data.jsp");
+//		mv.setViewName("data.jsp");
+		mv.addObject("un", "Mangi");
+		mv.addObject("ua", 32);
+
+		return mv;
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(HttpServletRequest request, Model model) {
+
+		String username = request.getParameter("un");
+		String password = request.getParameter("pwd");
+
+		if (username.equals("admin") && password.equals("admin@123")) {
+			// display home page and welcome user
+			model.addAttribute("username", username);
+			return "home.jsp";
+		} else {
+			// display login page with error
+			model.addAttribute("msg", "Invalid credentials");
+			return "login.jsp";
+		}
+	}
+
 }
